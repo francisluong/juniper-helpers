c5627f4ec9e8789735ff4a58f0a54b3bf921aa0b
Francis Luong (Franco) <franco@definefunk.com>
2014-01-06 17:44:14 -0500

    python/nc001_inventory.py: playing with ncclient

---
+++ python/nc001_inventory.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+class Lineprinter:
+  def __init__ (self):
+    self.value = 0
+  def incr(self):
+    self.value = self.value + 1
+  def section(self):
+    print "\n\n=== %d ===" % self.value
+    self.incr()
+lp = Lineprinter()
+
+###
+lp.section()
+#we use the juniper version of ncclient v0.1a from https://github.com/Juniper/ncclient
+from ncclient import manager
+from pprint import pprint as pp
+
+#for argv
+import sys, os
+
+def connect(host, username, port=830, password=None, timeout=10, hostkey_verify=False):
+  """
+    Return a netconf connection object
+     - prompt for password if None
+  """
+  if password is None: 
+    from getpass import getpass
+    password = getpass('{}@{} password: '.format(user, host))
+  #return jnpr.junos.Device(host, user=user, password=password).open()
+  return manager.connect(host=host, port=port, username=username, password=password,
+    timeout=timeout, hostkey_verify=hostkey_verify)
+
+if __name__ == '__main__':
+  if len(sys.argv) == 1:
+    print "usage: {} <host> [ <username> ]".format(sys.argv[0])
+  else:
+    if len(sys.argv) == 2:
+      user = os.getenv("USER")
+    else:
+      user = sys.argv[2]
+    host = sys.argv[1]
+    sess = connect(host, user)
+    print "Attributes of ncclient Session:"
+    pp(dir(sess))
+
+    lp.section()
+    result = sess.get_chassis_inventory()
+    print "Attrubites of {}".format(result)
+    pp(dir(result))
+
+    #lp.section()
+    #from lxml import etree
+    #root = etree.parse(result.data_xml)
+    #print etree.tostring(root, pretty_print=True)
+
+    lp.section()
+    print result.data_xml
+
+    lp.section()
+    print "Chassis:", result.xpath('//chassis/description')[0].text
+    print "Chassis Serial-Number:", result.xpath('//chassis/serial-number')[0].text
+    print "CB0 Serial Number:", result.xpath('//chassis/chassis-module[name="CB 0"]/serial-number')[0].text
+
+    lp.section()
+    from itertools import izip
+    print "Chassis Modules:"
+    names = result.xpath('//chassis/chassis-module/name')
+    serials = result.xpath('//chassis/chassis-module/serial-number')
+    parts = result.xpath('//chassis/chassis-module/model-number')
+    for name, serial, part in izip(names, serials, parts):
+      print " - {0}: {1} // {2}".format( name.text, part.text, serial.text )  
+
