0d73c7ec6001d74831d7491f8f3b603634b98732
Francis Luong (Franco) <franco@definefunk.com>
2014-01-14 22:50:11 -0500

    reorganized python folder

--- test/misc.tcl
+++ test/misc.tcl
@@ -3,6 +3,7 @@ set auto_path [linsert $auto_path 0 "/home/fluong/code/juniper-helpers"]
 package require output
 package require textproc
 
+#tcl allows you to have variables with default values
 proc blah {{text "default"}} {
   puts $text
 }
