38f5d292e28b7a9997378859c27d0e828a384944
Francis Luong (Franco) <franco@definefunk.com>
2014-03-09 05:56:07 -0400

    more YAML scratch... need to write a range generator

--- scratch/yaml_scratch.tcl
+++ scratch/yaml_scratch.tcl
@@ -1,5 +1,7 @@
 #!/usr/bin/env tclsh8.5
 
+package require output
+
 proc read_file {file_name_and_path} {
  ###########################################
  # abstract: reads in the contents of a file and returns a string
@@ -20,9 +22,25 @@ proc read_file {file_name_and_path} {
 }
 
 
+proc range {start, stop, {incrementBy 1}} {
+}
+
+
+puts [subst "YAML!!"]
 puts [package require yaml]
 set filepath "/home/fluong/juniper-helpers/scratch/test.yml"
-set yaml_in [yaml::yaml2dict [read_file $filepath]]
-puts $yaml_in
-puts "SRLGID: [dict get $yaml_in simple_substitutions SRLGID]"
-puts "keys: [dict keys $yaml_in]"
+set yaml_full [yaml::yaml2dict [read_file $filepath]]
+puts $yaml_full
+puts "keys: [dict keys $yaml_full]"
+foreach key [dict keys $yaml_full] {
+  puts "\n\n === $key ===\n"
+  set yaml_in [dict get $yaml_full $key]
+  puts $yaml_in
+  output::pdict yaml_in
+  puts "keys: [dict keys $yaml_in]"
+  foreach {varname value} [dict get $yaml_in simple_substitutions] {
+    set $varname $value
+  }
+  set config [dict get $yaml_in config]
+  puts "\nfinished config:\n[subst $config]"
+}
