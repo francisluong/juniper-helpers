6732a222bc40c92c097eed82f3408b7a22e10b64
Francis Luong (Franco) <franco@definefunk.com>
2014-04-03 21:01:41 -0400

    output_collector: moved stuff into namespace

--- examples/output_collector/run_collection.tcl
+++ examples/output_collector/run_collection.tcl
@@ -1,84 +1,7 @@
 #!/usr/bin/env tclsh
 
-package require JuniperConnect
-package require yaml
-package require output
+package require oc
 
-
-proc verify_options {options_dict} {
-  set pass 1
-  foreach key [list "path_main" "path_templates" "output_folder"] {
-    set filepath [file normalize [dict get $options_dict $key]]
-    if {([file isdirectory $filepath] && [file writable $filepath])} {
-      #good
-    } else {
-      puts stderr "Filepath $filepath doesn't exist or not writable"
-    }
-  }
-  if {!$pass} {
-    exit
-  }
-}
-
-proc verify_template {filepath template_dict} {
-  set valid 1
-  set keys_list [dict keys $template_dict]
-  foreach key [list "folder" "router_list_name" "commands"] {
-    if {[lsearch -exact $keys_list $key] == -1} {
-      set valid 0
-    }
-  }
-  if {$valid} {
-  } else {
-    puts " !! Template Failed Verify: $filepath"
-  }
-  return $valid
-}
-
-proc process_template {filepath template_dict options_dict} {
-  set valid [verify_template $filepath $template_dict]
-  if {$valid} {
-    output::pdict template_dict
-    dict with template_dict {
-      #add $output_folder to $folder
-      set folder "[dict get $options_dict "output_folder"]/$folder"
-      #create folder if it doesn't exist
-      file mkdir $folder
-      #if folder is writable we can continue
-      if {[file writable $folder]} {
-        puts " >> Processing Template: $filepath"
-        set routers_list [dict get $options_dict $router_list_name]
-        foreach router $routers_list {
-          #connect and get output
-          connectssh $router
-          set output_text [send_textblock $router $commands]
-          #write output to file
-          set fl [open "$folder/$router" w]
-          puts $fl [string trim $output_text]
-          close $fl
-          #disconnect session
-          disconnectssh $router
-        }
-      }  else {
-        print " !! Templated Failed: Folder not writable: $folder - template $filepath"
-      }
-    }
-  }
-}
-
-proc run_collection {options_dict} {
-  #now process files in path_templates
-  dict with options_dict {
-    foreach infile [glob $path_templates/*] {
-      set file_contents [read_file $infile]
-      #only process non-empty files
-      if {$file_contents ne ""} {
-        set template_dict [yaml::yaml2dict $file_contents]
-        process_template $infile $template_dict $options_dict
-      }
-    }
-  }
-}
 
 #usage
 if {$argc < 2} {
@@ -87,14 +10,12 @@ if {$argc < 2} {
 } 
 #read in userpass file
 import_userpass [lindex $argv 0]
-#read in etc/config.yml
+#read in config.yml
 set filepath_config_yml [lindex $argv 1]
-set options_dict [yaml::yaml2dict [read_file $filepath_config_yml]
-output::pdict options_dict
-#verify it
-verify_options $options_dict
+oc::load_config $filepath_config_yml
+
 #you can also 'dict set options_dict' to add router lists
-#dict set options_dict router_list_3 $xyz
+  #dict set oc::options_dict router_list_3 $xyz
 
 #now we run collection
-run_collection $options_dict
+oc::run_collection 
