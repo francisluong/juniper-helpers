479e4932e212838722ddd3b108438b47aacd50a5
Francis Luong (Franco) <fluong@juniper.net>
2014-04-07 23:33:29 -0400

    base64

--- scratch/mime_smtp.tcl
+++ scratch/mime_smtp.tcl
@@ -1,7 +1,9 @@
 #!/usr/bin/env tclsh
 
+package require Expect
 package require mime
 package require smtp
+package require base64
 
 #usage
 if {$argc < 1} {
@@ -10,18 +12,27 @@ if {$argc < 1} {
 }
 
 set email_address [lindex $argv 0]
-if {$argc > 2} {
+if {$argc > 1} {
   set body_message [lindex $argv 1]
 } else {
-  set body_message "trying out tcllib mime/smtp"
+  set body_message "trying out tcllib mime/smtp\nline 2\rline 3"
 }
 
 set csv "name, breed, color
 Scooby Doo, Great Dane, Brown
 Snoopy, Beagle, White"
-set message_token [mime::initialize -canonical "text/plain" -string $body_message]
-set csv_token [mime::initialize -canonical "text/plain; name=\"attach1.csv\"" -string $csv]
-set a2_token [mime::initialize -canonical "text/plain; name=\"attach2.txt\"" -string "attachment 2"]
+set message_token [mime::initialize \
+  -canonical "text/plain" \
+  -encoding base64 \
+  -string $body_message]
+set csv_token [mime::initialize \
+  -canonical "text/plain; name=\"attach1.csv\"" \
+  -encoding "base64" \
+  -string $csv]
+set a2_token [mime::initialize \
+  -canonical "text/plain; name=\"attach2.txt\"" \
+  -encoding "base64" \
+  -string "attachment 2"]
 
 set parts_list [list $message_token $csv_token $a2_token]
 
@@ -32,6 +43,9 @@ set smtp_result [smtp::sendmessage $multipart_token \
   -header [list To $email_address] \
   -header [list Subject "tcllib mime/smtp test -- [clock format [clock seconds]]"]]
 
+puts "To $email_address"
+puts "Subject tcllib mime/smtp test -- [clock format [clock seconds]]"
+puts "Body $body_message"
 puts "smtp_result: '$smtp_result'"
 
 mime::finalize $multipart_token -subordinates all
