d1585e239deabf72158f3172c5ce38f10866609d
Francis Luong (Franco) <franco@definefunk.com>
2014-04-11 00:25:38 -0400

    working on scratch/concurrency.tcl

--- scratch/concurrency.tcl
+++ scratch/concurrency.tcl
@@ -29,6 +29,10 @@
 # when starting a thread_iteration, it will spawn process as background process
 # until there are a number running equal to "max_threads"
 #
+# Thread_Iteration Proc
+# =====================
+#
+# should call "thread_finish" when it's done with everything
 # 
 #
 #Procs that need to be supplied - application specific
@@ -51,6 +55,10 @@
 # - 
 #
 
+package provide concurrency 1.0
+package require base32
+package require output
+
 namespace eval concurrency {
 
   variable max_threads 5
@@ -61,6 +69,8 @@ namespace eval concurrency {
   variable thread_iteration_procname {}
   variable iteration_match_text "RUN_THREAD_ITERATION"
   variable iteration_argv_index 0
+  variable tmp_folder "/var/tmp"
+  variable is_thread_iteration 0
 
   proc init {thread_iteration_procname} {
     global argc argv   
@@ -69,6 +79,7 @@ namespace eval concurrency {
     #if this is an iteration, run it and exit
     if {[lindex $argv $iteration_argv_index] eq $iteration_match_text} {
       #call thread_iteration and exit 
+      variable is_thread_iteration 1
       exit [$thread_iteration_procname [lindex $argv 1]]
     }
     #initialize things
@@ -82,10 +93,10 @@ namespace eval concurrency {
   proc process_queue {input_queue} {
     #initialize
     set concurrency::input_queue $input_queue
-    set complete 0
+    set queue_empty 0
     set queue_item 0
     #loop until we are done with the queue
-    while {!$complete} {
+    while {!$queue_empty} {
       #start a new thread if we can, if not... returns -1
       while {$queue_item != -1} {
         #started a new thread... try to start more until we get a return of -1
@@ -101,7 +112,7 @@ namespace eval concurrency {
         thread_finish ${queue_item}
       }
       #check to see if we are done with the queue
-      set complete [concurrency_complete]
+      set queue_empty [queue_is_empty]
     }
   }
 
@@ -113,37 +124,74 @@ namespace eval concurrency {
     if {[llength $concurrency::input_queue] != 0 && ($current_sessions < $concurrency::max_threads)} {
       #dequeue
       set this_item [lindex $concurrency::input_queue 0]
-      set concurrency::input_queue [lrange $concurrency(input_list) 1 end]
+      set concurrency::input_queue [lrange $concurrency::input_queue 1 end]
       #add to current_list
       lappend concurrency::current_queue $this_item
-      #bump current_sessions counter
-      incr concurrency(current_sessions)
     }
     return $this_item
   }
 
-  proc thread_finish {this_item} {
-    set index [lsearch -exact $concurrency::current_queue $this_item]
-    if {$index != -1} {
-      #match found
-      #delete this_item from current_list
-      set concurrency::current_queue [lreplace $concurrency::current_queue $index $index]
-      #add this_item to finished_list
-      set concurrency::finished_queue $this_item
-      return 1
-    } else {
-      #match not found
-      return 0
-    }
+  proc output_filepath {queue_item} {
+    set format_string "%G-%m%d"
+    set today [clock format [clock seconds] -format $format_string]
+    set ofilename [base32::encode $queue_item]
+    return "$concurrency::tmp_folder/$today.$ofilename.txt"
   }
 
-  proc concurrency_complete {} {
+  proc thread_start {queue_item} {
+    #runfile - path to thread script
+    #queue_item - the text of the concurrency queue item
+    set path [generate_results_path $routername]
+    puts "Start: $routername - $path"
+    variable iteration_match_text
+    set outfile [output_filepath $queue_item]
+    exec [info script] $iteration_match_text $queue_item $outfile >& /dev/null &
+  }
+
+  proc thread_finish {queue_item} {
+    set finished 0
+    set outfile [output_filepath $queue_item]
+    set ofilename [base32::encode $queue_item]
+    variable is_thread_iteration 
+    if {$is_thread_iteration} {
+      #is a thread_iteration
+      #output flag to indicate thread_iteration is complete
+      print "\n$ofilename"
+      return
+    } else {
+      #not a thread_iteration
+      #read file
+
+      #get last line
+
+      #if last line == $ofilename, thread_iteration is complete
+      if { $last_line eq $ofilename } {
+        set finished 1
+        set index [lsearch -exact $concurrency::current_queue $this_item]
+        if {$index != -1} {
+          #match found
+          #delete this_item from current_list
+          set concurrency::current_queue [lreplace $concurrency::current_queue $index $index]
+          #add this_item to finished_list
+          set concurrency::finished_queue $this_item
+        } else {
+          #match not found
+          return -code error "concurrency::thread_finish: dequeuing problem: $queue_item not found"
+        }
+      } else {
+        #not finished
+      }
+    }
+    return $finished
+  }
+
+  proc queue_is_empty {} {
     set input_empty 0
     set current_empty 0
-    if {[llength [concurrency_input_list]] == 0} {
+    if {[llength $concurrency::input_queue] == 0} {
       set input_empty 1
     }
-    if {[llength [concurrency_current_list]] == 0} {
+    if {[llength $concurrency::current_queue] == 0} {
       set current_empty 1
     }
     if {$input_empty && $current_empty} {
@@ -163,18 +211,6 @@ namespace eval concurrency {
 
   proc concurrency_finished_list {} {
     return $concurrency::finished_queue
-  }
-
-  proc generate_results_path {routername} {
-    return "/var/tmp/concurrency.tcl.${routername}.txt"
-  }
-
-  proc thread_start {runfile queue_item} {
-    #runfile - path to thread script
-    #queue_item - the text of the concurrency queue item
-    set path [generate_results_path $routername]
-    puts "Start: $routername - $path"
-    exec $runfile $routername $path >& /dev/null &
   }
 
   proc finish_collection {routername} {
